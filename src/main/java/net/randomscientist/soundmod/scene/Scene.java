c6a684407d46e19f5bc26a8db02b8097774496aa
The-Minecraft-Scientist <charlesliske@gmail.com>
2022-06-21 02:48:50 -0700

    work on scene loader some more (I just realized it's 3AM lol)

--- src/main/java/net/randomscientist/soundmod/scene/Scene.java
+++ src/main/java/net/randomscientist/soundmod/scene/Scene.java
@@ -3,6 +3,7 @@ package net.randomscientist.soundmod.scene;
 import net.minecraft.client.world.ClientChunkManager;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.chunk.WorldChunk;
 import net.randomscientist.soundmod.util.SceneChunk;
 import net.randomscientist.soundmod.util.Pos2i;
 
@@ -14,6 +15,7 @@ public class Scene {
     final PlayerEntity player;
     private int size = 4;
     final Pos2i baseChunk = new Pos2i(0,0);
+    Pos2i playerChunk = new Pos2i(0,0);
     private ArrayList<Pos2i> loadpool;
     private ArrayList<Pos2i> loaded;
     public Scene(ClientChunkManager cm, PlayerEntity player,int size) {
@@ -23,10 +25,9 @@ public class Scene {
     }
     public void update() {
         BlockPos playerPos = player.getBlockPos();
-        Pos2i playerChunk = new Pos2i(0,0);
-        playerChunk.setX(playerPos.getX()>>4);
-        playerChunk.setZ(playerPos.getZ()>>4);
-        if(!(playerChunk.getZ() == baseChunk.getZ() && playerChunk.getX() == baseChunk.getX())) {
+        playerChunk.x=playerPos.getX()>>4;
+        playerChunk.y=playerPos.getZ()>>4;
+        if(!(playerChunk.y == baseChunk.y && playerChunk.x == baseChunk.x)) {
             for(int i =baseChunk.x-size; i<baseChunk.x+size; i++) {
                 for(int j = baseChunk.y-size;j< baseChunk.y+size; j++) {
                     Pos2i chunk = new Pos2i(i,j);
@@ -35,6 +36,19 @@ public class Scene {
                     }
                 }
             }
+        }
+    }
+    private int posToIndex(Pos2i p) {
+        Pos2i rel=p.sub(playerChunk).add(new Pos2i(size,size));
+        return rel.y<<3|rel.x;    }
+    public void parse() {
+        if(!(loadpool.size()==0)) {
+            Pos2i task = loadpool.get(0);
+            int ind = posToIndex(task);
+            WorldChunk c = cm.getWorldChunk(task.x,task.y);
+            chunks[ind].ingestChunk(c);
+            loadpool.remove(0);
+            loaded.add(task);
         }
     }
 
